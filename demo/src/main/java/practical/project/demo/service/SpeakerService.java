b6030c62160f6c9646557505da8cb50a9f5011ad
orloveste <orlovevm@yahoo.com>
2020-12-13 14:16:28 +0200

    jpaRepository - CRUD SpeakerService

--- demo/src/main/java/practical/project/demo/service/SpeakerService.java
+++ demo/src/main/java/practical/project/demo/service/SpeakerService.java
@@ -1,8 +1,36 @@
 package practical.project.demo.service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import practical.project.demo.model.Session;
+
+import practical.project.demo.model.Speaker;
+import practical.project.demo.repository.SpeakerRepository;
+
+import java.util.List;
 
 @Service
 public class SpeakerService {
+    //trebuie sa injectam repository
+    @Autowired
+    SpeakerRepository speakerRepository;
+
+    //findAll
+    List<Speaker> findAllSpeakers(){
+        return speakerRepository.findAll();
+    }
+    //session dupa id
+    Speaker getSpeakerById(Integer id) {
+        return speakerRepository.getOne(id);
+    }
+    //create session
+    public Speaker createSpeaker(Speaker newSpeaker) {
+        return speakerRepository.saveAndFlush(newSpeaker);
+    }
+
+    //delete
+    public void deleteSpeakerById(Integer id) {
+        speakerRepository.deleteById(id);
+    }
 }
