83b88dff331862e16987746e0cc2fd05c3ba9219
orloveste <orlovevm@yahoo.com>
2020-12-18 22:45:43 +0200

    Speaker.java - undo demo

--- demo/src/main/java/practical/project/demo/model/Speaker.java
+++ demo/src/main/java/practical/project/demo/model/Speaker.java
@@ -1,22 +1,62 @@
 package practical.project.demo.model;
 
-import lombok.Data;
-
 import javax.persistence.*;
 import java.util.List;
 
-@Data
 @Entity(name = "speakers")
 public class Speaker {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int idSpeaker;//id for entry in postman
-    private String nameSpeaker;//id for entry in postman
-//    private Long  speakerId;
-//    private String speakerName;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long speakerId;
+
+    private String speakerName;
+    private String speakerFirstName;
+    private String speakerCompany;
+
 
     @ManyToMany(mappedBy = "speakers")
     List<Session> sessions;
-    public Speaker() {}
 
+    public Speaker() {
+    }
+
+    public Long getSpeakerId() {
+        return speakerId;
+    }
+
+    public void setSpeakerId(Long speakerId) {
+        this.speakerId = speakerId;
+    }
+
+    public String getSpeakerName() {
+        return speakerName;
+    }
+
+    public void setSpeakerName(String speakerName) {
+        this.speakerName = speakerName;
+    }
+
+    public String getSpeakerFirstName() {
+        return speakerFirstName;
+    }
+
+    public void setSpeakerFirstName(String speakerFirstName) {
+        this.speakerFirstName = speakerFirstName;
+    }
+
+    public String getSpeakerCompany() {
+        return speakerCompany;
+    }
+
+    public void setSpeakerCompany(String speakerCompany) {
+        this.speakerCompany = speakerCompany;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
 }
