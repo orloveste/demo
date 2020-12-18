82dacd7ae4ae981ae1e799d05e0266b0bf922891
orloveste <orlovevm@yahoo.com>
2020-12-18 22:33:37 +0200

    SpeakerController.java - undo demo

--- demo/src/main/java/practical/project/demo/controller/SessionJsonProjection.java
+++
@@ -1,9 +0,0 @@
-package practical.project.demo.controller;
-
-import lombok.Data;
-
-@Data
-public class SessionJsonProjection {
-    private Integer id_session;
-    private String name_session;
-}
--- demo/src/main/java/practical/project/demo/controller/SpeakerController.java
+++ demo/src/main/java/practical/project/demo/controller/SpeakerController.java
@@ -2,11 +2,8 @@ package practical.project.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import practical.project.demo.model.Session;
 import practical.project.demo.model.Speaker;
-import practical.project.demo.service.SessionService;
 import practical.project.demo.service.SpeakerService;
-
 import java.util.List;
 
 @RestController
@@ -15,34 +12,22 @@ public class SpeakerController {
     @Autowired
     SpeakerService speakerService;
     //get
-    /*@GetMapping
-    public List<Speaker> findAllSpeakers() {
-        return speakerService.findAllSpeakers();
-    }*/
     @RequestMapping(method = RequestMethod.GET)
     public List<Speaker> speakerList() {
         return speakerService.getAllSpeakers();
     }
     //get endpoint
-    /*@GetMapping
-    @RequestMapping("{id_Speaker}")
-    public Speaker finndSpeakerById(@PathVariable Integer idSpeaker) {
-        return speakerService.getSpeakerById(idSpeaker);
-    }*/
-    @RequestMapping(value = "{id}", method = RequestMethod.GET)//rev1
+    @RequestMapping(value = "/speaker/{id}", method = RequestMethod.GET)
     public Speaker getSpeakerById(@PathVariable Integer id) {
         return speakerService.getSpeakerById(id);
     }
-
-    @PostMapping//rev1
-    public Speaker addSpeaker(@RequestBody Speaker speaker) {
-        return speakerService/*.createSpeaker*/.addSpeaker (speaker);//createSession(session);
+    @RequestMapping(value = "/speaker", method = RequestMethod.POST)
+    public Speaker createSpeaker(@RequestBody Speaker speaker) {
+        return speakerService.addSpeaker(speaker);
     }
-
     //delete by id
-    /*@DeleteMapping*/ //rev1
-    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)/*_speaker*/
-    public void deleteSpeakerById (Integer id/*Speaker*/) {
-        speakerService.deleteSpeakerById(id/*Speaker*/);//sessionService.deleteSessionById(id);
+    @RequestMapping(value = "/speaker/{id}", method = RequestMethod.DELETE)
+    public void deleteSpeakerById (@PathVariable Integer id) {
+        speakerService.deleteSpeakerById(id);
     }
 }
